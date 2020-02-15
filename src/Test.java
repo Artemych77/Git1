9ab550f3850c35b8353f5ec87ad99597cb2b145b
Artem Morhun <bonykrol710@gmail.com>
2020-02-15 14:06:55 +0200

    Commit 1

--- src/Test.java
+++ src/Test.java
@@ -3,13 +3,13 @@ public class Test {
     public static void main(String[] args){
         int n0 = 1;
         int n1 = 1;
-        int n2;
+        int n;
         System.out.print(n0+" "+n1+" ");
         for(int i = 3; i <= 11; i++){
-            n2=n0+n1;
-            System.out.print(n2+" ");
+            n=n0+n1;
+            System.out.print(n+" ");
             n0=n1;
-            n1=n2;
+            n1=n;
         }
         System.out.println();
     }
