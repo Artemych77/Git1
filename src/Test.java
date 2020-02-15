a33ecffda0684f2c9d828bef277c3b7147dee294
Yaroslav <q.link0.p@gmail.com>
2020-02-15 13:47:24 +0200

    prettyfied things

--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,12 @@
-
 public class Test {
     public static void main(String[] args){
-        int n0 = 1;
+        int n0 = 0;
         int n1 = 1;
         int n2;
         System.out.print(n0+" "+n1+" ");
         for(int i = 3; i <= 11; i++){
             n2=n0+n1;
-            System.out.print(n2+" ");
+            System.out.print("The number is: " + n2 + " ");
             n0=n1;
             n1=n2;
         }
